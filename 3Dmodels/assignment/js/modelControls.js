3a42932c8c2a97a981db196337c42481d041e2b6
JamesStening <36426176+JamesStening@users.noreply.github.com>
2022-08-03 17:31:31 +0100

    Add files via upload

---
+++ 3Dmodels/assignment/js/modelControls.js
@@ -0,0 +1,92 @@
+function camera1()
+{
+	if (document.getElementById('model__Camera001').getAttribute('bind')) {
+		document.getElementById('model__Camera001').setAttribute('bind', 'false');
+	}
+	document.getElementById('model__Camera001').setAttribute('bind', 'true');
+}
+
+function camera2()
+{
+	if (document.getElementById('model__Camera002').getAttribute('bind')) {
+		document.getElementById('model__Camera002').setAttribute('bind', 'false');
+	}
+	document.getElementById('model__Camera002').setAttribute('bind', 'true');
+}
+
+function camera3()
+{
+	if (document.getElementById('model__Camera003').getAttribute('bind')) {
+		document.getElementById('model__Camera003').setAttribute('bind', 'false');
+	}
+	document.getElementById('model__Camera003').setAttribute('bind', 'true');
+}
+
+function enter()
+{
+	document.getElementById('model__Line002').setAttribute('translation', '0,0,0');	
+}
+
+
+var lights = new Array(6).fill(true);
+function light(l)
+{
+	if (l > 0)
+	{
+		changeL(l);
+	}
+	else
+	{
+		for (let l = 1; l < lights.length; l++)
+		{
+			lights[l] = false;
+			changeL(l);
+		}
+	}
+	
+}
+
+function changeL(l)
+	{
+		lights[l] = !lights[l];
+		document.getElementById('model__Omni00' + l.toString()).setAttribute('on', lights[l] );
+		if (lights[l])
+		{
+			//document.getElementById('l' + l.toString()).setAttribute('style', "border:1px solid black; background-color: FloralWhite");
+			document.getElementById('l' + l.toString()).setAttribute('class', "btn lightButtOn")
+		}
+		else
+		{
+			//document.getElementById('l' + l.toString()).setAttribute('style', "border:1px solid black; background-color: LightGrey");
+			document.getElementById('l' + l.toString()).setAttribute('class', "btn lightButtOff")
+		}
+	}
+
+function texture()
+{
+	document.getElementById('model__Line002-Texture').setAttribute('url', '"../sceneassets\\\\images/can_texture2.jpg"' );//document.getElementById('model3D');
+}
+
+function animation()
+{
+	document.getElementById('model__Line002-TIMER').setAttribute('startTime', 0)
+}
+
+function animationStart()
+{
+	document.getElementById('model__Line002-TIMER').setAttribute('cycleInterval', 3.333)
+	document.getElementById('model__Line002-TIMER').setAttribute('startTime', 0)
+}
+
+function animationSlow()
+{
+	document.getElementById('model__Line002-TIMER').setAttribute('cycleInterval', 6.666)
+	document.getElementById('model__Line002-TIMER').setAttribute('startTime', 0)
+}
+
+
+
+
+
+
+
